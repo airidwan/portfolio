e2f36229f73eebb7b83f6d85712d75c231fa6dd3
ridwanwong <a.ridwanudin@outlook.com>
2022-06-12 23:42:42 +0700

    remove unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
-// import { ImBlog } from "react-icons/im";
 import {
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
@@ -89,16 +88,7 @@ function NavBar() {
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
-{/* 
-            <Nav.Item>
-              <Nav.Link
-                href="http://www.logicstime.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item> */}
+
           </Nav>
         </Navbar.Collapse>
       </Container>
